e46a1f3abe5b39120ca527e13915842671c0827d
Anthony Guzman <anthonyguzman@MacBook-Air-de-Anthony.local>
2023-02-25 20:55:17 -0300

    filter correction

--- client/src/components/filters/filters.jsx
+++ client/src/components/filters/filters.jsx
@@ -22,6 +22,7 @@ const Filters = () => {
 
     useEffect(() => {
         dispatch(getCountryByName(inputSeachByName))
+        dispatch(getPages(1))
     }, [dispatch, inputSeachByName])
 
     const homeHandler = () => {
