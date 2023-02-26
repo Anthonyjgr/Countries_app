c8ac5304f93986cf58f02a67df127ea53534f550
Anthony Guzman <anthonyguzman@MacBook-Air-de-Anthony.local>
2023-02-26 15:21:55 -0300

    landing changes

--- client/src/views/landing/landing.jsx
+++ client/src/views/landing/landing.jsx
@@ -4,29 +4,26 @@ import github_black from "../../assets/github_black.png"
 import Style from "./landing.module.css"
 import { Link } from "react-router-dom"
 
-
 const Landing = () => {
     return (
         <div className={Style.container}>
-
-            {/* <img className={style.background} src={travel_sales} alt="background_image" /> */}
-
-            <div className={Style.centralBox}>
-                <div className={Style.txtContainer}>
-                <p className={Style.txt1}>DO YOU</p>
-                <p className={Style.txt2}>like</p>
-                <p className={Style.txt1}>GEOGRAPHY</p>
-                <p className={Style.txt2}>and</p>
-                <p className={Style.txt1}>TOURISM?</p>
+            <div className={Style.ContainercentralBox}>
+                <div className={Style.leftBox}>
+                    <div className={Style.txtContainer}>
+                        <p className={Style.txt}>LEARN MORE ABOUT COUNTRIES AND SHARING YOUR EXPERIENCES TO OTHER PEOPLE.</p>
+                    </div>
                 </div>
-               
-                <div className={Style.buttonContainer}>
-                    <Link to="/home">
-                        <button className={Style.button}><div className={Style.yes}><p>!YES!</p></div></button>
-                    </Link>
+                <div className={Style.rightBox}>
+                    <div className={Style.containerButton}>
+                        <Link to="/home">
+                            <button className={Style.button}>LET'S GO STARTED</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-            <div className={Style.separador}></div>
+
+            <div className={Style.lines1}></div>
+
             <div className={Style.iconsContainer}>
                 <a
                     href="https://www.linkedin.com/in/anthony-guzman-840449135/"
@@ -35,7 +32,6 @@ const Landing = () => {
                 >
                     <img className={Style.png} src={linkedin_black} alt="linkedin" />
                 </a>
-
                 <a
                     href="https://www.instagram.com/antho.guitar/"
                     target="_blank"
@@ -43,7 +39,6 @@ const Landing = () => {
                 >
                     <img className={Style.png} src={instagram_black} alt="instagram" />
                 </a>
-
                 <a
                     href="https://github.com/Anthonyjgr"
                     target="_blank"
@@ -52,6 +47,7 @@ const Landing = () => {
                     <img className={Style.png} src={github_black} alt="github" />
                 </a>
             </div>
+            <div className={Style.lines}></div>
         </div >
     )
 };
